8e8712620bbf39674b72f888b73af7c045e5a312
letsdie10 <sngby98@gmail.com>
2018-02-03 16:48:22 +0000

    added websites of stuff

--- run_main.sh
+++ run_main.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# https://www.cs.uic.edu/~jbell/CourseNotes/C_Programming/Arrays.html
+# https://stackoverflow.com/questions/18548157/c-header-files-and-compilation-linking
+
 gcc -c main.c
 gcc -c sortAndShuffle.c
 gcc sortAndShuffle.o main.o -o main
